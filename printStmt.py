706204901f46d9079c0fe7a884163c583bf300da
Akriti Srivastava26 <akrsri26@in.ibm.com>
2021-06-27 03:16:58 +0530

    first

---
+++ printStmt.py
@@ -0,0 +1,8 @@
+myName = "Akriti"
+name = "Parul"
+age = "30"
+print("My name is " + myName + " and my age is " + str(age) + ".")
+print("Her name is " + name + ".")
+myName = "Diana"
+print("My name is " + myName + " and my age is " + str(age) + ".")
+

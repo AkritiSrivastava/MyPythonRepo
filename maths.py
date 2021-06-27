94f4e77295f2fed29380100553807d02040b2c26
AkritiSrivastava <iti.astonish@gmail.com>
2021-06-27 18:37:48 +0530

    maths code

---
+++ maths.py
@@ -0,0 +1,15 @@
+from math import *
+
+print(-338721.59)
+print(3 * (4 + 5))
+print(10%3)
+num = -18.2
+print("Absolute value of -10.5 = " + str(abs(-10.5)))
+print("Absolute value of -18.2 = " + str(abs(num)))
+print(pow(3,2))
+print(max(14837598,74658475))
+print(min(27364,9847))
+print(round(234.39))
+print(ceil(346.45))
+print(floor(3742.89))
+print(sqrt(36))
